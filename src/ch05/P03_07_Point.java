ab17e2d6894196e9f9ef3e33ee96873ac1e8343f
AA <180617768+jhshin23@users.noreply.github.com>
2025-07-23 14:21:51 +0900

    Page322, P04, overloading, getDistance()

--- src/ch05/P03_07_Point.java
+++ src/ch05/P03_07_Point.java
@@ -24,7 +24,32 @@ class ColorPoint extends Point {
 		return getColor() + "색의 (" + getX() + "," + getY() + ")의 점"; 
 	}
 }
-public class P03_07_Point {
+class ColorPoint2 extends Point {
+	private String color;
+	public ColorPoint2() {
+		super(0, 0);
+		color = "WHITE";
+	}
+	public ColorPoint2(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public ColorPoint2(int x, int y) {
+		super(x, y);
+		color = "BLACK";
+	}
+	
+	public void set(int x, int y) { move(x, y); }
+	public void set(String color) { this.color = color; }
+	public String getColor() { return color; }
+	public String toString() {
+		return getColor() + "색의 (" + getX() + "," + getY() + ")의 점"; 
+	}
+	public double getDistance(ColorPoint2 thresholdPoint) {
+		return Math.sqrt((this.getX() - thresholdPoint.getX()) * (this.getX() - thresholdPoint.getX()) + (this.getY() - thresholdPoint.getY()) * (this.getY() - thresholdPoint.getY()));
+	}
+}
+	public class P03_07_Point {
 
 	public static void main(String[] args) {
 		ColorPoint cp = new ColorPoint(5, 5, "RED");
@@ -32,6 +57,20 @@ public class P03_07_Point {
 		cp.setColor("BLUE");
 		String str = cp.toString();
 		System.out.println(str + "입니다.");
+		
+		ColorPoint2 zeroPoint = new ColorPoint2();
+		System.out.println(zeroPoint.toString() + "입니다.");
+		
+		ColorPoint2 cp2 = new ColorPoint2(10, 10, "RED");
+		
+		cp2.set("BLUE");
+		cp2.set(10, 20);
+		System.out.println(cp2.toString() + "입니다.");
+		
+		ColorPoint2 thresholdPoint = new ColorPoint2(100, 100);
+		
+		System.out.println("cp2에서 임계점까지의 거리는 " + cp2.getDistance(thresholdPoint));
+		
 	}
 
 }
